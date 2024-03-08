0467e6bcc056f364c8f87639838d60fb812ca12e
Moises Uy <it.moisesuy@gmail.com>
2024-03-08 14:44:22 +0800

    Boxxed the Shoplist

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <h1 className="text-white">Hello</h1>
       <p className="text-white">Lorem 100</p>
+      <div className="bg-black w-3/5 h-5"></div>
     </>
   );
 }
--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -11,46 +11,46 @@ type ShopItem = {
 export default function Shop() {
   return (
     <>
-      <SearchBar />
-      <div className="grid grid-cols-3 gap-8">
-        {SHOP_ITEMS.map((item, id) => (
-          <div
-            key={id}
-            className="flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl"
-          >
-            <div>
-              <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
+      <div className="w-3/5">
+        <div className="grid grid-cols-3 gap-8 ">
+          {SHOP_ITEMS.map((item, id) => (
+            <div
+              key={id}
+              className="flex flex-col text-gray-700 bg-white shadow-2xl bg-clip-border rounded-xl"
+            >
+              <div className="outline -outline-offset-1 rounded-t-xl h-40">
                 <img
                   src={item.path}
                   alt={item.name}
-                  className="object-cover w-full h-full"
+                  className="object-cover w-full h-full rounded-t-xl"
                 />
               </div>
-            </div>
-            <div className="p-6">
-              <div className="flex items-center justify-between mb-2">
-                <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                  {item.name}
-                </p>
-                <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-                  {`$` + item.price}
+              <div className="p-6">
+                <div className="flex items-center justify-between mb-2">
+                  <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                    {item.name}
+                  </p>
+                  <p className="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
+                    {`$` + item.price}
+                  </p>
+                </div>
+                <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
+                  {item.desc}
                 </p>
               </div>
-              <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
-                {item.desc}
-              </p>
+              <div className="p-4 pt-0">
+                <button
+                  className="align-middle bg-slate-800 rounded-full p-3 text-white"
+                  type="button"
+                >
+                  Add to Cart
+                </button>
+              </div>
             </div>
-            <div className="p-4 pt-0">
-              <button
-                className="align-middle bg-slate-800 rounded-full p-3 text-white"
-                type="button"
-              >
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <section>Hello</section>
     </>
   );
 }
